a67c459d0dc752558479acfb05ce38296644f8b2
Raviteja1652 <ravitejamrt.165@gmail.com>
2023-12-30 12:22:29 +0530

    viewing the full mail

--- src/components/Mail/Inbox.js
+++ src/components/Mail/Inbox.js
@@ -1,19 +1,46 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Context from '../store/Context'
+import './Inbox.css';
+
+const FullMessage = ({mailData, onBackClick}) => {
+    return (
+        <div>
+          <button onClick={onBackClick}>&larr; Back</button>
+          <h2>{mailData.subject}</h2>
+          <p>{mailData.message}</p>
+        </div>
+    );
+}
 
 const Inbox = () => {
-    const ctx = useContext(Context)
+    const ctx = useContext(Context);
+    const [selectedInbox, setSelectedInbox] = useState(null);
+    const [viewMode, setViewMode] = useState('list');
+
+    const inboxClickHandler = (id_mail) => {
+        const selectedMail = ctx.inboxMails.find(mail => mail.id === id_mail);
+        setSelectedInbox(selectedMail);
+        setViewMode('full');
+    };
+    const handleBackClick = () => {
+        setSelectedInbox(null);
+        setViewMode('list')
+    };
     const inboxList = ctx.inboxMails.map(mail => (
-        <li key={mail.id}>
+        <li key={mail.id} className='inbox-mail' onClick={() => inboxClickHandler(mail.id)}>
             <h5>From: {mail.senderMail}</h5>
             <span>{mail.subject} - {mail.message}</span>
         </li>
-    ))
+    ));
+    const inboxCount = ctx.inboxMails.length;
   return (
-    <ul>
-        {inboxList}
-    </ul>
+    <div>
+        <h6>{inboxCount} mails</h6>
+        {viewMode === 'list' && <ul>{inboxList}</ul>}
+        {viewMode === 'full' && selectedInbox && <FullMessage mailData={selectedInbox} onBackClick={handleBackClick} />}
+    </div>
+    
   )
 }
 
-export default Inbox
+export default Inbox;
