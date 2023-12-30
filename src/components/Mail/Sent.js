43ae52020cd2a6723a92ca90ae41d0d8dea92f69
Raviteja1652 <ravitejamrt.165@gmail.com>
2023-12-30 13:30:15 +0530

    viewing full sent mail

--- src/components/Mail/Sent.js
+++ src/components/Mail/Sent.js
@@ -1,18 +1,49 @@
-import React, { useContext } from 'react'
-import Context from '../store/Context'
+import React, { useContext, useState } from 'react'
+import Context from '../store/Context';
+import './Sent.css';
+
+const FullSentMessage = ({mailData, onBackClick}) => {
+  return (
+    <div>
+      <button onClick={onBackClick}>&larr; Back</button>
+      <h2>{mailData.subject}</h2>
+      <p>{mailData.message}</p>
+    </div>
+  );
+}
 
 const Sent = () => {
-    const ctx = useContext(Context)
+    const ctx = useContext(Context);
+    const [selectedSent, setSelectedSent] = useState(null);
+    const [viewMode, setViewMode] = useState('list');
+
+    const sentClickHandler = (id_mail) => {
+      const clickedMail = ctx.sentMails.find(mail => mail.id === id_mail);
+      setSelectedSent(clickedMail);
+      setViewMode('full');
+    };
+
+    const handleBackClick = () => {
+      setSelectedSent(null);
+      setViewMode('list')
+    };
+
     const sentMails = ctx.sentMails.map(mail => (
-        <li key={mail.id}>
+        <li key={mail.id} className='sent-mail' onClick={() => sentClickHandler(mail.id)}>
             <h5>To: {mail.recipientMail}</h5>
             <span>{mail.subject} -- {mail.message}</span>
         </li>
     ))
+    const sentCount = ctx.sentMails.length;
   return (
-    <ul>
-        {sentMails}
-    </ul>
+    <div>
+      <h5>{sentCount} sent mails</h5>
+      {viewMode === 'list' && <ul>{sentMails}</ul>}
+      {viewMode === 'full' && selectedSent && (
+        <FullSentMessage mailData={selectedSent} onBackClick={handleBackClick} />
+      )}
+    </div>
+    
   )
 }
 
